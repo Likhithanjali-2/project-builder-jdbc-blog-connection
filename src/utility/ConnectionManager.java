75520ebf54170dd11ec6787ac9d972e6612004b6
Likhithanjali-2 <liki2444@gmail.com>
2020-04-06 10:24:04 -0500

    first commit-

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,4 +1,24 @@
 package utility;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-
+public class ConnectionManager {
+	public Connection getConnection() throws Exception {
+		Properties prop = loadPropertiesFile();
+		Class.forName((String) prop.get("driver"));// can also use getProperety method to access
+		Connection con = DriverManager.getConnection((String) prop.get("url"),(String) prop.get("username"),(String) prop.get("password"));
+		return con;
+	}
+	
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+}
